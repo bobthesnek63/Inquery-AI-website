40cef1091740669547d6ef6c99bdd7e59c6f33d2
bobthesnek63 <56647593+bobthesnek63@users.noreply.github.com>
2020-05-26 23:17:38 -0400

    Add files via upload

---
+++ InqAI website/index.js
@@ -0,0 +1,16 @@
+function fade(){
+  var x = document.getElementById('logo');
+  x.style.opacity = 1
+}
+
+function Contact(){
+  console.log(1);
+}
+
+function work(){
+  window.open('work.html');
+}
+
+function team(){
+  window.open("team.html");
+}
